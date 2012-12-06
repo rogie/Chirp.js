511884ae7bf810a54ee014f670cdce4cbfe1e4e4
Rogie King <hug@rog.ie>
2012-12-06 13:32:25 -0700

    Fixed time issue where tweets older than a month would lose their time ago
    Signed-off-by: Rogie King <hug@rog.ie>

--- chirp.js
+++ chirp.js
@@ -38,17 +38,19 @@ var Chirp = function( opts ){
 			var date = new Date((time || "").replace(/(\d{1,2}[:]\d{2}[:]\d{2}) (.*)/, '$2 $1').replace(/(\+\S+) (.*)/, '$2 $1').replace(/-/g,"/")),
 				diff = (((new Date()).getTime() - date.getTime()) / 1000),
 				day_diff = Math.floor(diff / 86400);			
-			if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 31 )
+			if ( isNaN(day_diff) || day_diff < 0)
 				return;
 			return day_diff == 0 && (
 					diff < 60 && "just now" ||
 					diff < 120 && "1 minute ago" ||
-					diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
+					diff < 3600 && Math.floor(diff/60) + " minutes ago" ||
 					diff < 7200 && "1 hour ago" ||
-					diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
-				day_diff == 1 && "Yesterday" ||
-				day_diff < 7 && day_diff + " days ago" ||
-				day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
+					diff < 86400 && Math.floor(diff/3600) + " hours ago") ||
+				  day_diff == 1 && "Yesterday" ||
+				  day_diff < 7 && day_diff + " days ago" ||
+				  day_diff < 31 && Math.ceil(day_diff/7) + " weeks ago" ||
+				  day_diff < 365 && Math.ceil(day_diff/30) + " months ago" ||
+				  day_diff >= 365 && Math.ceil(day_diff/365) + " year" + (Math.ceil(day_diff/365)>1?"s":"") + " ago";
 		},
 		htmlify = function( txt, entities ){
 	    var indices = [],
